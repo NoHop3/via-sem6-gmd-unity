9715396c021d5a4d57b9b9bbcc1868dfed4b2c74
NoHop3 <u_sux@abv.bg>
2023-04-20 17:44:58 +0200

    Stalker now attacks correctly

--- Assets/Scripts/StalkerAI.cs
+++ Assets/Scripts/StalkerAI.cs
@@ -16,6 +16,11 @@ public class StalkerAI : MonoBehaviour
     public bool StalkerIsBeingHit = false;
     public bool StalkerIsHitting = false;
     public bool StalkerDead = false;
+    public AudioSource Hurt1;
+    public AudioSource Hurt2;
+    public AudioSource Hurt3;
+    public int RandomHurt;
+    public GameObject TheFlash;
 
     // Animatior constants
     const string CROUCHED_WALKING = "Crouched Walking";
@@ -47,11 +52,10 @@ public class StalkerAI : MonoBehaviour
         {
             NavAgent.SetDestination(transform.position);
         }
-        if (StalkerActive && !StalkerDead && !StalkerIsBeingHit)
+        if (StalkerActive && !StalkerDead && !StalkerIsBeingHit && !StalkerIsHitting)
         {
-            if (!AttackTrigger && !StalkerIsHitting)
+            if (!AttackTrigger)
             {
-                // NavAgent.SetActive(true);
                 NavAgent.SetDestination(StalkerDest.transform.position);
                 ChangeAnimationState(CROUCHED_WALKING);
             }
@@ -60,7 +64,7 @@ public class StalkerAI : MonoBehaviour
                 StartCoroutine(StalkerHit());
             }
         }
-        else if (!StalkerActive && !StalkerIsBeingHit)
+        else if (!StalkerActive && !StalkerIsBeingHit && !StalkerIsHitting)
         {
             NavAgent.SetDestination(transform.position);
             ChangeAnimationState(MUTANT_IDLE);
@@ -102,15 +106,32 @@ public class StalkerAI : MonoBehaviour
 
     IEnumerator StalkerHit()
     {
-        StalkerIsHitting = true;
-        ChangeAnimationState(MUTANT_PUNCH);
-        yield return new WaitForSeconds(1.1f);
-        Debug.Log(GlobalHealth.CurrentHealth);
-        // Fix stalekr dealing too much damage
-        StalkerIsHitting = false;
-        if (AttackTrigger)
+        if(AttackTrigger)
         {
-            GlobalHealth.CurrentHealth -= 0;
+            StalkerIsHitting = true;
+            ChangeAnimationState(MUTANT_PUNCH);
+            RandomHurt = Random.Range(1, 4);
+            switch (RandomHurt)
+            {
+                case 1:
+                    Hurt1.Play();
+                    break;
+                case 2:
+                    Hurt2.Play();
+                    break;
+                case 3:
+                    Hurt3.Play();
+                    break;
+            }
+            yield return new WaitForSeconds(1.1f);
+            TheFlash.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+            TheFlash.SetActive(false);
+            yield return new WaitForSeconds(1.5f);
+            GlobalHealth.CurrentHealth -= 5;
+            yield return new WaitForSeconds(1f);
+            StalkerIsHitting = false;
         }
+        else yield break;
     }
 }
